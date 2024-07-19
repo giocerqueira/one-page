45b23c91b0ddf2b26b578cd52327e998e8488ba7
Giovanna <giovannacerqueira.09@gmail.com>
2024-07-18 22:03:56 -0300

    responsividade para telas extra grandes

--- one-page/src/components/style.jsx
+++ one-page/src/components/style.jsx
@@ -12,7 +12,9 @@ export const Body = styled.body `
             font-size: 1.2em;
             width: 50vw;
             margin-top: 2rem;
-            /* border: solid blue; */
+        }
+        @media (min-width: 1550px) {
+            margin-top: 2rem;
         }
     }
 `
@@ -20,32 +22,83 @@ export const Body = styled.body `
 export const Main = styled.main`
     display: flex;
     width: 100%;
-    color: #2D2424;
-
-
+    color: #5C3D2E;
     p {
         margin-left: 1rem;
-        /* border: solid red; */
         font-size: 1em;
         font-weight: 400;
         margin-top: 1.2rem;
+        @media (min-width: 1550px) {
+            margin-left: 2rem;
+        }
     }
+
 `
 export const DivParagrafo = styled.div`
-    /* border: solid red; */
-    margin-top: 2.2rem;
+    margin-top: 1.5rem;
     width: 65vw;
+    height: 83vh;
     h2 {
         font-size: 1em;
         width: 60vw;
         height: 16vh;
         font-weight: 400;
-        /* margin-top: 0.2rem; */
         margin-left: 1rem;
-        /* border: solid blue; */
+        @media (min-width: 1550px) {
+            margin-top: 0rem;
+            height: 10vh;
+            margin-left: 2rem;
+        }
     }
-    /* border: solid blue; */
-    /* width: 30vw; */
+    div {
+        margin-left: 1rem;
+        display: flex;
+        width: 40vw;
+        height: 5vh;
+        margin-top: 0.5rem;
+        justify-content: space-between;
+        button {
+            border-radius: 1rem;
+            border: 2px solid #2D2424; 
+            background-color: #AF8F6F;
+            cursor: pointer;
+            margin-top: 0.5rem;
+            &:hover {
+                transform: scale(1.05); 
+                box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3); 
+            }
+            @media (min-width: 1550px) {
+                    height: 4vh;
+                    margin-top: 3rem;
+                    margin-left: 1.2rem;
+            }
+            a {
+                background-color: transparent;
+                color: #E5E5E5;
+                text-decoration: none;
+            }
+        }
+        #Github {
+            width: 13vw;
+            @media (min-width: 1550px) {
+                    width: 10.5vw;
+            }
+        }
+        #Repositorio {
+            width: 17.5vw;
+            @media (min-width: 1550px) {
+                    width: 14vw;
+            }
+        }
+    }
+
+    @media (min-width: 1550px) {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 80vh;
+        margin-top: 0rem;
+        }
 `
 
 export const P = styled.p`
@@ -53,23 +106,23 @@ export const P = styled.p`
 `
 export const P2 = styled.p`
     width: 62vw;
+    @media (min-width: 1550px) {
+            width: 60vw;
+        }
 `
 export const P3 = styled.p`
-    width: 58vw;
+    width: 58vw; 
 `
 
 export const DivImg = styled.div `
-    /* border: solid blue; */
     width: 36vw;
-
     img {
         width: 36vw;
         height: 81vh;
-        /* border: solid green; */
-        /* margin-top: 4rem; */
         position: absolute;
-        /* bottom: 0.1rem; */
         right: 0rem;
-        /* bottom: 50px; */
+        @media (min-width: 1550px) {
+            width: 38vw;
+        }
     }
 `
--- one-page/src/main.jsx
+++ one-page/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 import * as S from './components/style.jsx'
-import Image from './components/images/picture.png'
+import Image from './components/images/picture-copia.png'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,7 +22,10 @@ function Main() {
           <S.P>Como a formação já tinha um viés social (pois atendia somente mulheres de baixa renda), eu lidei com diversas pessoas, de realidades diferentes da minha, durante toda essa caminhada. Na formação, conheci mulheres que já são mães, donas de casa, que não deixaram seus sonhos morrerem e estão em busca de recolocação e transição de carreira. Porém, além disso, no hackathon eu tive a oportunidade de conhecer o Giovanni: o Giovanni é uma pessoa não-binária, que tem os pronomes ele/dele, e que foi um dos meus parceiros nessa competição.</S.P>
           <S.P2>O objetivo da competição era desenvolver um Portal do Paciente, lá ele deveria conseguir: se cadastrar; conferir seus dados pessoais e alterar, se necessário; agendar, reagendar e cancelar consultas; ver histórico de consultas canceladas e agendadas. Ao longo do desenvolvimento, o Giovanni trouxe vários insights valiosos, que me fizeram refletir sobre a importância da inclusão de pessoas não-binárias para coisas que, nós que somos cis nem imaginamos, como na hora de desenvolver um site. Isso porque é importante que a experiência do usuário seja positiva e, para isso, o site deve se comunicar com todos e todes. Digo isso porque eu fui responsável por desenvolver a página de cadastro do paciente e o tempo todo estive em contato com o Giovanni para deixar o formulário de cadastro da maneira mais inclusiva possível, pois tínhamos como foco manter o projeto acessível. Esse olhar dele foi muito importante para o desenvolvimento do projeto e o respeito mútuo também contribuiu muito para que o projeto tenha sido o sucesso que foi, pois, com ele, vencemos a competição.</S.P2>
           <S.P3>Logo, finalizo dizendo que, além da parte técnica e do contato com novas tecnologias, oportunidade de aprendizado e de crescimento, a Globo também tem muito a me agregar não só em hard skills, mas também em soft skills, me dando a oportunidade de conviver com pessoas de outras realidades além da minha e de aprender com a diferença. Além disso, acredito que eu também possa contribuir para a empresa, trazendo meus insights da minha realidade e estando sempre aberta para ouvir e aprender, assim como fiz durante a competição, para que juntos possamos fazer projetos completos, inclusivos e que tenham reconhecimento dentro da empresa.</S.P3>
-          <p>Github:</p>
+          <div>
+            <button id='Github'><a target="_blank" href="https://github.com/giocerqueira">Acesse aqui o meu Github!</a></button>
+            <button id='Repositorio'><a target="_blank" href="https://github.com/giocerqueira/one-page">Confira aqui o repositório desta página!</a></button>
+          </div>
         </S.DivParagrafo>
         <S.DivImg>
           <img src={Image} alt="" />
